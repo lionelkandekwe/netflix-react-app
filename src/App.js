8ae1fc3dc04bac6913090ded198cd33b47677b21
Lionel Kandekwe <lionelkandekwe@Lionels-MacBook-Pro.local>
2022-03-11 15:18:29 +0200

    Get movies & row component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import "./App.css"
+import Row from "./Components/Row"
+import requests from "./Config/requests"
 
 function App() {
   return (
     <div className="App">
       <h1>Netflix-Home Page</h1>
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+      />
+      <Row title="Trending now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   )
 }
