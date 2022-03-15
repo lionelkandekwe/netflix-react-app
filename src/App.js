4b6e49d0ddc431d764e4b089bdcd27b50eda4d55
Lionel Kandekwe <lionelkandekwe@Lionels-MacBook-Pro.local>
2022-03-15 10:14:52 +0200

    Build & style  NavBar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css"
 import Row from "./Components/Row"
 import Banner from "./Components/Banner"
+import Nav from "./Components/Nav"
 import requests from "./Config/requests"
 
 function App() {
   return (
     <div className="app">
+      <Nav />
       <Banner />
       <Row
         title="NETFLIX ORIGINALS"
